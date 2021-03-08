c5d631b1cc55baf679fed8daadea661b84e4efaf
Pugsly99 <rylandadams@yahoo.com>
2021-03-08 15:14:21 -0800

    firebase start for contact us

--- src/components/About/OurPlan.js
+++ src/components/About/OurPlan.js
@@ -10,22 +10,21 @@ margin-bottom: 120px;
 const Body = styled.div`
 text-align: center;
 font-size: 25px;
-font-family: "Times New Roman"!important;
+margin-left: 25%;
+margin-right: 25%;
 `
 
 
 function OurPlan() {
   return (
     <>
-      <Header >
-        <h1>Our Plan</h1> 
-      </Header>
-      <br></br>
-      <Body >
-      <h2>asdasdasdsad</h2>
-      
-      </Body>
-
+        <Header >
+          <h1>Our Plan</h1> 
+        </Header>
+        <br></br>
+        <Body >
+        <h2>Our goal is to build a team of creatives from all walks of life to support and grow together! I always say this, If a sculptor says they are going to put out a display it may attract 5 people. Now if you have 10 sculptors all in one place, you may get 100 people all of which came to support their artist but fell in love with another. This gives the best and quickest way for rapid growth as an artist in the modern world.</h2>
+        </Body>
     </>
   );
 }
